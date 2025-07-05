b51b8c7446bc73cd2351ed52d9e6929d4bc0578c
Arsenty Dafintsev <aaapython24@gmail.com>
2025-07-05 23:28:25 +0300

    Add CRUD for MailMessage and MailingRecipient models

--- mailer/urls.py
+++ mailer/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from mailer.apps import MailerConfig
 from .views import HomeTemplateView, MailingListView, MailingDetailView, MailingCreateView, MailingUpdateView, \
-    MailingDeleteView
+    MailingDeleteView, MessageListView, MessageDetailView, MessageCreateView, MessageUpdateView, MessageDeleteView, \
+    RecipientListView, RecipientDetailView, RecipientCreateView, RecipientUpdateView, RecipientDeleteView
 
 app_name = MailerConfig.name
 
@@ -9,7 +10,17 @@ urlpatterns = [
     path('', HomeTemplateView.as_view(), name='home'),
     path("mailing_list/", MailingListView.as_view(), name="mailing_list"),
     path("mailing/<int:pk>/", MailingDetailView.as_view(), name="mailing_detail"),
-    path("create/", MailingCreateView.as_view(), name="mailing_create"),
+    path("mailing_create/", MailingCreateView.as_view(), name="mailing_create"),
     path("mailing/<int:pk>/update/", MailingUpdateView.as_view(), name="mailing_update"),
     path("mailing/<int:pk>/delete/", MailingDeleteView.as_view(), name="mailing_delete"),
+    path("message_list/", MessageListView.as_view(), name="message_list"),
+    path("message/<int:pk>/", MessageDetailView.as_view(), name="message_detail"),
+    path("message_create/", MessageCreateView.as_view(), name="message_create"),
+    path("message/<int:pk>/update/", MessageUpdateView.as_view(), name="message_update"),
+    path("message/<int:pk>/delete/", MessageDeleteView.as_view(), name="message_delete"),
+    path("recipient_list/", RecipientListView.as_view(), name="recipient_list"),
+    path("recipient/<int:pk>/", RecipientDetailView.as_view(), name="recipient_detail"),
+    path("recipient_create/", RecipientCreateView.as_view(), name="recipient_create"),
+    path("recipient/<int:pk>/update/", RecipientUpdateView.as_view(), name="recipient_update"),
+    path("recipient/<int:pk>/delete/", RecipientDeleteView.as_view(), name="recipient_delete"),
 ]
--- mailer/views.py
+++ mailer/views.py
@@ -3,7 +3,7 @@ from django.urls import reverse_lazy, reverse
 from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from mailer.forms import MailingForm
-from mailer.models import Mailing
+from mailer.models import Mailing, MailMessage, MailingRecipient
 
 
 class HomeTemplateView(TemplateView):
@@ -22,12 +22,7 @@ class MailingDetailView(LoginRequiredMixin, DetailView):
 
 class MailingCreateView(LoginRequiredMixin, CreateView):
     model = Mailing
-    #    template_name = "blog/blog_form.html"
-    #    fields = ("title", "content", "preview")
     form_class = MailingForm
-
-    # field_to_exclude = ("status", "owner")
-
     success_url = reverse_lazy("mailer:mailing_list")
 
     def form_valid(self, form):
@@ -39,9 +34,6 @@ class MailingCreateView(LoginRequiredMixin, CreateView):
 class MailingUpdateView(LoginRequiredMixin, UpdateView):
     model = Mailing
     form_class = MailingForm
-    #template_name = "mailer/mailing_form.html"
-    #fields = ("title", "content", "preview")
-    #field_to_exclude = ("status", "owner")
     success_url = reverse_lazy("mailer:mailing_list")
 
     def get_success_url(self):
@@ -52,3 +44,88 @@ class MailingDeleteView(LoginRequiredMixin, DeleteView):
     model = Mailing
     context_object_name = "mailing"
     success_url = reverse_lazy("mailer:mailing_list")
+
+
+# AAAAAAAAAAAAAAAAAAAAAA
+
+class MessageListView(ListView):
+    model = MailMessage
+    context_object_name = 'messages'
+    template_name = "mailer/message_list.html"
+
+
+class MessageDetailView(LoginRequiredMixin, DetailView):
+    model = MailMessage
+    context_object_name = "message"
+    template_name = "mailer/message_detail.html"
+
+
+class MessageCreateView(LoginRequiredMixin, CreateView):
+    model = MailMessage
+    fields = ("title", "content")
+    template_name = "mailer/message_form.html"
+    success_url = reverse_lazy("mailer:message_list")
+
+    def form_valid(self, form):
+        user = self.request.user
+        form.instance.owner = user
+        return super().form_valid(form)
+
+
+class MessageUpdateView(LoginRequiredMixin, UpdateView):
+    model = MailMessage
+    fields = ("title", "content")
+    template_name = "mailer/message_form.html"
+    success_url = reverse_lazy("mailer:message_list")
+
+    def get_success_url(self):
+        return reverse("mailer:message_detail", args=[self.kwargs.get("pk")])
+
+
+class MessageDeleteView(LoginRequiredMixin, DeleteView):
+    model = MailMessage
+    context_object_name = "message"
+    template_name = "mailer/message_confirm_delete.html"
+    success_url = reverse_lazy("mailer:message_list")
+
+# ##### BBBBBBBBBBBBB
+#
+class RecipientListView(ListView):
+    model = MailingRecipient
+    context_object_name = 'recipients'
+    template_name = "mailer/recipient_list.html"
+
+
+class RecipientDetailView(LoginRequiredMixin, DetailView):
+    model = MailingRecipient
+    context_object_name = "recipient"
+    template_name = "mailer/recipient_detail.html"
+
+
+class RecipientCreateView(LoginRequiredMixin, CreateView):
+    model = MailingRecipient
+    fields = ("email", "full_name", "comment")
+    template_name = "mailer/recipient_form.html"
+    success_url = reverse_lazy("mailer:recipient_list")
+
+    def form_valid(self, form):
+        user = self.request.user
+        form.instance.owner = user
+        return super().form_valid(form)
+
+
+class RecipientUpdateView(LoginRequiredMixin, UpdateView):
+    model = MailingRecipient
+    fields = ("email", "full_name", "comment")
+    template_name = "mailer/recipient_form.html"
+    success_url = reverse_lazy("mailer:recipient_list")
+
+    def get_success_url(self):
+        return reverse("mailer:recipient_detail", args=[self.kwargs.get("pk")])
+
+
+class RecipientDeleteView(LoginRequiredMixin, DeleteView):
+    model = MailingRecipient
+    context_object_name = "recipient"
+    template_name = "mailer/recipient_confirm_delete.html"
+    success_url = reverse_lazy("mailer:recipient_list")
